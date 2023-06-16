65ab99e8086505d99ad46cf6dcffd1fa32c7e70e
Дарья Шарипова <darya.sharipova@oldcodex.ru>
2023-06-16 20:32:46 +0500

    выполнено 15 задание

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿StreamReader read_random_numbers = new StreamReader("random_numbers.txt");
+var line = read_random_numbers.ReadLine();
+List<string> numbers = new List<string>();
+Console.WriteLine("Обрабатываю данные");
+while(line != null)
+{
+    var splitLine = line.Split(" ");
+    if(numbers.Count == 0)
+    {
+        numbers.Add(splitLine[0]);
+        Console.Write(".");
+    }
+    else if(numbers.Count !=0)
+    {
+        for(int i =0; i< splitLine.Length; i++)
+        {
+            var number = splitLine[i];
+            if(number != " ")
+            {
+                if(!numbers.Contains(number))
+                {
+                    numbers.Add(number);
+                    Console.Write(".");
+                }
+            }
+            
+        }
+    }
+    line = read_random_numbers.ReadLine();
+}
+read_random_numbers.Close();
+List<int> intNumbers = new List<int>();
+for(int i =0; i< numbers.Count; i++)
+{
+    intNumbers.Add(int.Parse(numbers[i]));
+}
+intNumbers.Sort(); 
+Console.WriteLine("\nОбработка данных завершена");
+Console.WriteLine($"Всего вариант: {numbers.Count}");
+Console.Write($"Варианты: ");
+for(int i = 0; i< intNumbers.Count; i++)
+{
+    Console.Write(intNumbers[i] + " ");
+}
+Console.WriteLine();
